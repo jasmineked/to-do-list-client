bcbfe798479608b7575f014f055ec448c36d8a35
jasmineked <jked630@icloud.com>
2020-11-12 16:48:58 -0500

    updated production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://rocky-harbor-89349.herokuapp.com/',
+  production: 'https://rocky-harbor-89349.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
